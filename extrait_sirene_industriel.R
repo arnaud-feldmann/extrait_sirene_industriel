1669cbae8891c8531d31bbf62ff616b25bcd7173
arnaud-feldmann <arnaud.feldmann@gmail.com>
2024-05-22 09:20:46 +0000

    filtre tranches

--- extrait_sirene_industriel.R
+++ extrait_sirene_industriel.R
@@ -11,39 +11,51 @@ download.file("https://www.data.gouv.fr/fr/datasets/r/0651fb76-bcf3-4f6a-a38d-bc
 unzip(etabs,
       exdir = here("input"))
 
-activites_industrielles <- c("18.13Z" = "Activités de pré-presse",
-                             "18.12Z" = "Autre imprimerie (labeur)",
-                             "10.71C" = "Boulangerie et boulangerie-pâtisserie",
-                             "33.20C" = "Conception d'ensemble et assemblage sur site industriel d'équipements de contrôle des processus industriels",
-                             "30.20Z" = "Construction de locomotives et d'autre matériel ferroviaire roulant",
-                             "25.50B" = "Découpage, emboutissage",
-                             "29.32Z" = "Fabrication d'autres équipements automobiles",
-                             "28.29B" = "Fabrication d'autres machines d'usage général",
-                             "25.73Z" = "Fabrication d'autres outillages",
-                             "28.23Z" = "Fabrication de machines et d'équipements de bureau (à l'exception des ordinateurs et équipements périphériques",
-                             "27.12Z" = "Fabrication de matériel de distribution et de commande électrique",
-                             "32.50A" = "Fabrication de matériel médico-chirurgical et dentaire",
-                             "28.11Z" = "Fabrication de moteurs et turbines, à l'exception des moteurs d'avions et de véhicules",
-                             "27.11Z" = "Fabrication de moteurs, génératrices et transformateurs électriques",
-                             "20.42Z" = "Fabrication de parfums et de produits pour la toilette",
-                             "21.20Z" = "Fabrication de préparations pharmaceutiques",
-                             "26.40Z" = "Fabrication de produits électroniques grand public",
-                             "14.14Z" = "Fabrication de vêtements de dessous",
-                             "14.13Z" = "Fabrication de vêtements de dessus",
-                             "26.30Z" = "Fabrication d'équipements de communication",
-                             "28.29A" = "Fabrication d'équipements d'emballage, de conditionnement et de pesage",
-                             "29.31Z" = "Fabrication d'équipements électriques et électroniques automobiles",
-                             "26.51B" = "Fabrication d'instrumentation scientifique et technique",
-                             "10.71A" = "Fabrication industrielle de pain et de pâtisserie fraîche",
-                             "18.11Z" = "Imprimerie de journaux",
-                             "33.20B" = "Installation de machines et équipements mécaniques",
-                             "25.62B" = "Mécanique industrielle",
-                             "24.45Z" = "Métallurgie des autres métaux non ferreux",
-                             "10.71D" = "Pâtisserie",
-                             "10.13A" = "Préparation industrielle de produits à base de viande",
-                             "25.61Z" = "Traitement et revêtement des métaux",
-                             "10.83Z" = "Transformation du thé et du café",
-                             "10.11Z" = "Transformation et conservation de la viande de boucherie")
+activites_industrielles <-
+  tribble(~activitePrincipaleEtablissement, ~activitePrincipaleEtablissementLabel,
+          "18.13Z", "Activités de pré-presse",
+          "18.12Z", "Autre imprimerie (labeur)",
+          "10.71C", "Boulangerie et boulangerie-pâtisserie",
+          "33.20C", "Conception d'ensemble et assemblage sur site industriel d'équipements de contrôle des processus industriels",
+          "30.20Z", "Construction de locomotives et d'autre matériel ferroviaire roulant",
+          "25.50B", "Découpage, emboutissage",
+          "29.32Z", "Fabrication d'autres équipements automobiles",
+          "28.29B", "Fabrication d'autres machines d'usage général",
+          "25.73Z", "Fabrication d'autres outillages",
+          "28.23Z", "Fabrication de machines et d'équipements de bureau (à l'exception des ordinateurs et équipements périphériques",
+          "27.12Z", "Fabrication de matériel de distribution et de commande électrique",
+          "32.50A", "Fabrication de matériel médico-chirurgical et dentaire",
+          "28.11Z", "Fabrication de moteurs et turbines, à l'exception des moteurs d'avions et de véhicules",
+          "27.11Z", "Fabrication de moteurs, génératrices et transformateurs électriques",
+          "20.42Z", "Fabrication de parfums et de produits pour la toilette",
+          "21.20Z", "Fabrication de préparations pharmaceutiques",
+          "26.40Z", "Fabrication de produits électroniques grand public",
+          "14.14Z", "Fabrication de vêtements de dessous",
+          "14.13Z", "Fabrication de vêtements de dessus",
+          "26.30Z", "Fabrication d'équipements de communication",
+          "28.29A", "Fabrication d'équipements d'emballage, de conditionnement et de pesage",
+          "29.31Z", "Fabrication d'équipements électriques et électroniques automobiles",
+          "26.51B", "Fabrication d'instrumentation scientifique et technique",
+          "10.71A", "Fabrication industrielle de pain et de pâtisserie fraîche",
+          "18.11Z", "Imprimerie de journaux",
+          "33.20B", "Installation de machines et équipements mécaniques",
+          "25.62B", "Mécanique industrielle",
+          "24.45Z", "Métallurgie des autres métaux non ferreux",
+          "10.71D", "Pâtisserie",
+          "10.13A", "Préparation industrielle de produits à base de viande",
+          "25.61Z", "Traitement et revêtement des métaux",
+          "10.83Z", "Transformation du thé et du café",
+          "10.11Z", "Transformation et conservation de la viande de boucherie") %>%
+  as_arrow_table()
+
+tranches_concernees <-
+  tribble(
+    ~trancheEffectifsEtablissement, ~trancheEffectifsEtablissementLabel,
+    "21", "50 à 99 salariés",
+    "22", "100 à 199 salariés",
+    "31", "200 à 249 salariés",
+    "32", "250 à 499 salariés"
+  )
 
 sirene <-
   here("input", "StockEtablissement_utf8.csv") %>%
@@ -60,15 +72,16 @@ sirene <-
                                      enseigne3Etablissement = string(),
                                      denominationUsuelleEtablissement = string()),
                  as_data_frame = FALSE) %>%
+  mutate(dep =  str_sub(codePostalEtablissement, 1L, 2L)) %>%
   filter((is.na(etatAdministratifEtablissement) | etatAdministratifEtablissement == "A") &
            trancheEffectifsEtablissement != "NN" &
-           str_sub(codePostalEtablissement, 1L, 2L) %in% c("75", "77", "78", "91", "92", "93", "94", "95") &
-           activitePrincipaleEtablissement %in% names(activites_industrielles)) %>%
-  mutate(across(starts_with("enseigne"), ~if_else(is.na(.x),"",.x))) %>%
-  mutate(enseigneEtablissement = str_c(enseigne1Etablissement,
+           dep %in% c("75", "77", "78", "91", "92", "93", "94", "95")) %>%
+  inner_join(tranches_concernees, by = "trancheEffectifsEtablissement") %>%
+  inner_join(activites_industrielles, by = "activitePrincipaleEtablissement") %>%
+  mutate(across(starts_with("enseigne"), ~if_else(is.na(.x),"",.x)),
+         enseigneEtablissement = str_c(enseigne1Etablissement,
                                        enseigne2Etablissement,
                                        enseigne3Etablissement)) %>%
-  select(-c(etatAdministratifEtablissement,
-            enseigne1Etablissement,
-            enseigne2Etablissement,
-            enseigne3Etablissement))
+  select(c(siret, siren, trancheEffectifsEtablissementLabel, activitePrincipaleEtablissementLabel,
+           enseigneEtablissement, denominationUsuelleEtablissement)) %>%
+  compute()
